f3553200d28140410d7dbc16e7121ce4de561a3b
Nothing <lajizhongzhuanzhan@git.com>
2016-10-31 21:29:56 +0800

    second

--- src/com/company/Test3.java
+++ src/com/company/Test3.java
@@ -8,6 +8,7 @@ public class Test3 {
         if (args.length == 0) {
             return;
         }
+
         LocalDate ld = LocalDate.parse(args[0]);
         if (ld.getDayOfWeek() == DayOfWeek.FRIDAY && ld.getDayOfMonth() == 13) {
             System.out.println("YES");
